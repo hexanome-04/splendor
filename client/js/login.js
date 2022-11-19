5424b1da2fd2959afcf6c6e984cf989fb055bc90
Alex <17598972+sandpipes@users.noreply.github.com>
2022-11-19 15:25:30 -0500

    Implement proper login (#54)
    * Remove unnecessary form element (we dont post to anything)
    
    * Add basic logic to make a functional login page
    
    * Update url to be valid
    
    * Revert "Remove unnecessary form element (we dont post to anything)"
    
    This reverts commit b51a9b62d5866edb1a6d9997552f121a0c0f662a.
    
    * Implement set and get for tokens
    
    * Finish implementing login (communication to LS)
    
    * Set LS url to server hosted online
    
    * Fix issues with invalid shake and login page

--- client/js/login.js
+++ client/js/login.js
@@ -1,8 +1,54 @@
 
+var inputs = document.querySelectorAll(".center input, .center button");
+var centerDiv = document.querySelector(".center");
+var enableInputs = () => inputs.forEach(e => e.disabled = false);
+var disableInputs = () => inputs.forEach(e => e.disabled = true);
 
 document.querySelector("body").onload = () => {
-    document.querySelector(".login-button button").onclick = () => {
-        location.href = "lobby.html";
-        console.log("hi");
-    };
+    document.querySelector("form").addEventListener("submit", (event) => {
+        disableInputs();
+        centerDiv.classList.remove("invalid");
+
+        const username = document.querySelector(".txt-field input[name='uname']").value;
+        const password = document.querySelector(".txt-field input[name='psw']").value;
+        const params = {
+            "grant_type": "password",
+            "username": username,
+            "password": password
+        };
+        const headers = new Headers();
+        headers.set("Authorization", `Basic ${btoa("bgp-client-name:bgp-client-pw")}`);
+
+        const url = new URL(`${window.SETTINGS.LS_API}/oauth/token`);
+        url.search = new URLSearchParams(params).toString();
+
+        fetch(url, {
+            method: "POST",
+            headers: headers
+        })
+            .catch((reason) => {
+                // catch exceptions here
+
+                // console.log(reason);
+                // show user error message
+                window.alert(reason);
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                // console.log(data);
+
+                if("error" in data) {
+                    centerDiv.classList.add("invalid");
+                    window.alert(data.error_description);
+                    return;
+                }
+
+                window.SETTINGS.setAccessToken(data.access_token);
+                window.SETTINGS.setRefreshToken(data.refresh_token);
+
+                location.href = "lobby.html";
+            })
+            .finally(() => enableInputs());
+        event.preventDefault();
+    });
 };
---
+++ client/js/settings.js
@@ -0,0 +1,48 @@
+// Define settings to be used, that are shared across the frontend
+
+/**
+ * Define the settings onto the window, can't really define it as global.
+ * Linter will complain.
+ */
+
+window.SETTINGS = {
+    /**
+     * URL to access the Lobby Service
+     */
+    LS_API: "http://ls.hoshi.tv:54172",
+
+    /**
+     * Get the access token stored in local storage
+     * @returns string | null
+     */
+    getAccessToken: () => {
+        // can add check here to ensure that there's an access token
+        // if not, can boot client to login screen if expired or just not theree
+
+        return localStorage.getItem("accessToken");
+    },
+
+    /**
+     * Store the access token in local storage
+     * @param {string} token 
+     */
+    setAccessToken: (token) => {
+        localStorage.setItem("accessToken", token);
+    },
+
+    /**
+     * Get the refresh token stored in local storage
+     * @returns string | null
+     */
+    getRefreshToken: () => {
+        return localStorage.getItem("refreshToken");
+    },
+
+    /**
+     * Store the refresh token in local storage
+     * @param {string} token 
+     */
+    setRefreshToken: (token) => {
+        localStorage.setItem("refreshToken", token);
+    }
+};
