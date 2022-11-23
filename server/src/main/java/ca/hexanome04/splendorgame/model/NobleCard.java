87f386f3c0b246f15a014fe5e804ea77a9d5194a
Richard Rassokhine <74204667+richardrxn@users.noreply.github.com>
2022-11-22 21:01:40 -0500

    fixed checkstyle violations on master and Noble extends Card now (#65)
    Co-authored-by: rullebrett <74204667+rullebrett@users.noreply.github.com>

--- server/src/main/java/ca/hexanome04/splendorgame/model/NobleCard.java
+++ server/src/main/java/ca/hexanome04/splendorgame/model/NobleCard.java
@@ -1,29 +1,22 @@
 package ca.hexanome04.splendorgame.model;
 
+import java.util.ArrayList;
+
 /**
  * Class representing the noble cards.
  */
-public class NobleCard implements CardType {
-    private final int prestigePoints;
+public class NobleCard extends Card implements CardType {
 
     /**
-     * NobleCard constructor that instantiate a noble card on the board when it is drawn.
-     * 
-     * @param prestigePoints Amount of prestige points associated with this noble.
+     * Creates a new Noble card with provided prestige point amount, cost type and card type.
+     *
+     * @param prestigePoints Amount of prestige points associated to this card.
+     * @param costType       Cost type associated to this card.
+     * @param cardType       Card type associated to this card.
+     * @param tokenCost      Token cost associated to this card.
      */
-
-    public NobleCard(int prestigePoints) {
-        this.prestigePoints = prestigePoints;
-    }
-
-    /**
-     * getPrestigePoints will return the amount of prestige points a noble card has.
-     * 
-     * @return The amount of prestige points to add to the player's count.
-     */
-
-    public int getPrestigePoints(){
-        return prestigePoints;
+    public NobleCard(int prestigePoints, CostType costType, CardType cardType, ArrayList<TokenType> tokenCost) {
+        super(prestigePoints, costType, cardType, tokenCost);
     }
 }
 
