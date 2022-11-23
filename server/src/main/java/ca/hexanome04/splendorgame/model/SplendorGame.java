7cf0fc6227ab63ae1445d45ea652085bee347194
Chen Jun Chi <93779005+MosinLover@users.noreply.github.com>
2022-11-23 12:07:49 -0500

    Update SplendorGame.java
    implemented constructor and a method to get the next player to play

--- server/src/main/java/ca/hexanome04/splendorgame/model/SplendorGame.java
+++ server/src/main/java/ca/hexanome04/splendorgame/model/SplendorGame.java
@@ -8,8 +8,40 @@ import java.util.ArrayList;
 public class SplendorGame {
 
     private SplendorBoard boardState;
-
     private ArrayList<Player> players = new ArrayList<>();
+    private final int prestigePointsToWin;
+    private int turnId;
 
+/**
+ * Creates a Splendor Game with the given board state, the number of prestige points to win and the ordered list of the players.
+ * 
+ * @param boardState The state of the gameboard.
+ * @param prestigePointsToWin The amount of prestige points needed to win the game.
+ * @param players The player order in the game.
+ */
+    public SplendorGame(SplendorBoard boardState, int prestigePointsToWin, ArrayList<Player> players, int turnId){
+        this.boardState = boardState;
+        this.prestigePointsToWin = prestigePointsToWin;
+        this.players.addAll(players);        
+        this.turnId = turnId;
+    }
+
+    /**
+     * 
+     * @return the next player who is going to play. 
+     */
+
+    public Player getNextPlayer(){
+        if(turnId == 3){
+            Player nextPlayer = players.get(turnId);
+            turnId = 0;
+            return nextPlayer;
+        }else{
+            Player nextPlayer = players.get(turnId);
+            turnId++;
+            return nextPlayer;
+        }
+        
+    }
 
 }
