24ecbc5f24b0bdb769219aed40246b81adcf39a3
syouinou <112899577+syouinou@users.noreply.github.com>
2023-03-18 17:56:09 -0400

    Make Player an interface (#258)
    * Cities win condition wip
    
    * Remove toString

--- server/src/main/java/ca/hexanome04/splendorgame/model/Player.java
+++ server/src/main/java/ca/hexanome04/splendorgame/model/Player.java
@@ -8,86 +8,42 @@ import java.util.Map;
 /**
  * Class that represents a player.
  */
-public abstract class Player {
-
-    private String name;
-    private String colour;
-
-    private int prestigePoints;
-    private HashMap<TokenType, Integer> tokens;
-    private HashMap<TokenType, Integer> bonuses;
-    private ArrayList<DevelopmentCard> devCards;
-    private ArrayList<NobleCard> nobleCards;
-    private ArrayList<DevelopmentCard> reservedCards;
-    private ArrayList<NobleCard> reservedNobles;
-
-    /**
-     * Creates a Player object with a given name and colour.
-     *
-     * @param name   The name of the player.
-     * @param colour The color associated to the player.
-     */
-    public Player(String name, String colour) {
-        this.name = name;
-        this.colour = colour;
-        prestigePoints = 0;
-        tokens = new HashMap<>();
-        bonuses = new HashMap<>();
-        devCards = new ArrayList<>();
-        reservedCards = new ArrayList<>();
-        reservedNobles = new ArrayList<>();
-        nobleCards = new ArrayList<>();
-
-        for (TokenType tokenType : TokenType.values()) {
-            tokens.put(tokenType, 0);
-            bonuses.put(tokenType, 0);
-        }
-    }
+public interface Player {
 
     /**
      * Get player name.
      *
      * @return Returns name of player.
      */
-    public String getName() {
-        return name;
-    }
+    String getName();
 
     /**
      * Get player colour.
      *
      * @return Returns colour of player.
      */
-    public String getColour() {
-        return colour;
-    }
+    String getColour();
 
     /**
      * Set player name.
      *
      * @param name The player's new name.
      */
-    public void setName(String name) {
-        this.name = name;
-    }
+    void setName(String name);
 
     /**
      * Set player colour.
      *
      * @param colour The player's new colour.
      */
-    public void setColour(String colour) {
-        this.colour = colour;
-    }
+    void setColour(String colour);
 
     /**
      * Adds tokens to player inventory.
      *
      * @param tokens Tokens to add to player inventory.
      */
-    public void addTokens(HashMap<TokenType, Integer> tokens) {
-        tokens.forEach((key, value) -> this.tokens.put(key, this.tokens.get(key) + value));
-    }
+    void addTokens(HashMap<TokenType, Integer> tokens);
 
     /**
      * Adds bonuses to player inventory.
@@ -95,37 +51,28 @@ public abstract class Player {
      * @param bonus to add to player inventory.
      * @param numBonuses how many of this bonus to be added to player inventory.
      */
-    public void addBonus(TokenType bonus, int numBonuses) {
-        this.bonuses.put(bonus, bonuses.get(bonus) + numBonuses);
-    }
+    void addBonus(TokenType bonus, int numBonuses);
 
     /**
      * Adds noble card to player inventory.
      *
      * @param nobleCard Noble to add to player inventory
      */
-    public void addNoble(NobleCard nobleCard) {
-        this.nobleCards.add(nobleCard);
-        this.prestigePoints += 3;
-    }
+    void addNoble(NobleCard nobleCard);
 
     /**
      * Adds prestige points to player score.
      *
      * @param prestigePoints Prestige points to add to player
      */
-    public void addPrestigePoints(int prestigePoints) {
-        this.prestigePoints += prestigePoints;
-    }
+    void addPrestigePoints(int prestigePoints);
 
     /**
      * Adds card to player inventory.
      *
      * @param card Card to be added to player inventory
      */
-    public void addCard(DevelopmentCard card) {
-        devCards.add(card);
-    }
+    void addCard(DevelopmentCard card);
 
     /**
      * Removes a card from player inventory.
@@ -133,93 +80,70 @@ public abstract class Player {
      * @param card Card to removed from player inventory
      */
 
-    public void removeCard(DevelopmentCard card) {
-        this.devCards.remove(card);
-    }
+    void removeCard(DevelopmentCard card);
 
     /**
      * Removes a reserved development card from player inventory.
      *
      * @param card Card to removed from player inventory
      */
-    public void removeReservedCard(DevelopmentCard card) {
-        this.reservedCards.remove(card);
-    }
+    void removeReservedCard(DevelopmentCard card);
 
     /**
      * Remove a reserved noble from the players inventory.
      *
      * @param card reserved noble card
      */
-    public void removeReservedNoble(NobleCard card) {
-        this.reservedNobles.remove(card);
-    }
+    void removeReservedNoble(NobleCard card);
 
     /**
      * Get a player's held tokens.
      *
      * @return list of tokens that the player has
      */
-    public HashMap<TokenType, Integer> getTokens() {
-        return new HashMap<>(this.tokens);
-    }
+    HashMap<TokenType, Integer> getTokens();
 
     /**
      * Get a player's owned nobles.
      *
      * @return list of nobles that the player has
      */
-    public ArrayList<NobleCard> getNobles() {
-        return new ArrayList<>(this.nobleCards);
-    }
+    ArrayList<NobleCard> getNobles();
 
     /**
      * Get a player's reserved cards.
      *
      * @return list of reserved cards that the player has
      */
-    public List<DevelopmentCard> getReservedCards() {
-        return new ArrayList<>(this.reservedCards);
-    }
+    List<DevelopmentCard> getReservedCards();
 
     /**
      * Get a player's reserved nobles.
      *
      * @return list of reserved nobles that the player has
      */
-    public List<NobleCard> getReservedNobles() {
-        return new ArrayList<>(this.reservedNobles);
-    }
+    List<NobleCard> getReservedNobles();
 
     /**
      * Get a player's bonuses (obtained from their owned development cards).
      *
      * @return map of player's bonuses
      */
-    public Map<TokenType, Integer> getBonuses() {
-        return new HashMap<>(this.bonuses);
-    }
+    Map<TokenType, Integer> getBonuses();
 
     /**
      * Removes tokens from player inventory.
      *
      * @param tokens Tokens to remove from player inventory.
      */
-    public void removeTokens(HashMap<TokenType, Integer> tokens) {
-        tokens.forEach((key, value) -> this.tokens.put(key, this.tokens.get(key) - value));
-    }
+    void removeTokens(HashMap<TokenType, Integer> tokens);
 
     /**
      * Removes bonuses from player inventory.
      *
      * @param bonuses Bonuses to remove from player inventory
      */
-    public void removeBonuses(HashMap<TokenType, Integer> bonuses) {
-        bonuses.forEach((key, value) -> {
-            Integer currentValue = this.bonuses.get(key);
-            this.bonuses.put(key, currentValue - value);
-        });
-    }
+    void removeBonuses(HashMap<TokenType, Integer> bonuses);
 
     /**
      * Takes / puts back tokens for the player's turn.
@@ -227,28 +151,21 @@ public abstract class Player {
      * @param take    The tokens the player wishes to take during this turn.
      * @param putBack The tokens the player wishes to put back this turn.
      */
-    public void takeTokens(HashMap<TokenType, Integer> take, HashMap<TokenType, Integer> putBack) {
-        addTokens(take);
-        removeTokens(putBack);
-    }
+    void takeTokens(HashMap<TokenType, Integer> take, HashMap<TokenType, Integer> putBack);
 
     /**
      * Add card to reserved cards for a player and add one gold token to inventory (for their turn).
      *
      * @param card The development card the player would like to reserve.
      */
-    public void reserveCard(DevelopmentCard card) {
-        reservedCards.add(card);
-    }
+    void reserveCard(DevelopmentCard card);
 
     /**
      * Add card to reserved cards for a player and add one gold token to inventory (for their turn).
      *
      * @param noble The development card the player would like to reserve.
      */
-    public void reserveNoble(NobleCard noble) {
-        reservedNobles.add(noble);
-    }
+    void reserveNoble(NobleCard noble);
 
     /**
      * Check if player has the tokens specified in the given list.
@@ -256,18 +173,7 @@ public abstract class Player {
      * @param checkTokens tokens to be checked
      * @return true if player has the specified tokens
      */
-    public boolean hasTokens(HashMap<TokenType, Integer> checkTokens) {
-
-        for (TokenType key : checkTokens.keySet()) {
-
-            if ((this.tokens.get(key) - checkTokens.get(key)) < 0) {
-                return false;
-            }
-
-        }
-        return true;
-
-    }
+    boolean hasTokens(HashMap<TokenType, Integer> checkTokens);
 
     /**
      * Check if player has the bonuses specified in the given list.
@@ -275,32 +181,21 @@ public abstract class Player {
      * @param checkBonuses bonuses to be checked
      * @return true if player has the specified bonuses
      */
-    public boolean hasBonuses(HashMap<TokenType, Integer> checkBonuses) {
-        for (TokenType key : checkBonuses.keySet()) {
-            if ((this.bonuses.get(key) - checkBonuses.get(key)) < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+    boolean hasBonuses(HashMap<TokenType, Integer> checkBonuses);
 
     /**
      * Check if player has at least one noble.
      *
      * @return true if player has at least one noble
      */
-    public boolean hasNobles() {
-        return (this.nobleCards.size() > 0);
-    }
+    boolean hasNobles();
 
     /**
      * Returns the arraylist of cards that a player has.
      *
      * @return the arraylist of cards that a player has
      */
-    public List<DevelopmentCard> getDevCards() {
-        return new ArrayList<DevelopmentCard>(this.devCards);
-    }
+    List<DevelopmentCard> getDevCards();
 
     /**
      * Retrieve a purchased development card from the players inventory using the card id.
@@ -308,15 +203,7 @@ public abstract class Player {
      * @param cid card id
      * @return development card if found
      */
-    public DevelopmentCard getPurchasedDevelopmentCard(String cid) {
-        for (DevelopmentCard c : this.devCards) {
-            if (c.getId().equals(cid)) {
-                return c;
-            }
-        }
-        return null;
-
-    }
+    DevelopmentCard getPurchasedDevelopmentCard(String cid);
 
     /**
      * Retrieve a reserved development card from the players inventory using the card id.
@@ -324,27 +211,13 @@ public abstract class Player {
      * @param cid card id
      * @return development card if found
      */
-    public DevelopmentCard getReservedDevelopmentCard(String cid) {
-        for (DevelopmentCard c : this.reservedCards) {
-            if (c.getId().equals(cid)) {
-                return c;
-            }
-        }
-        return null;
-    }
+    DevelopmentCard getReservedDevelopmentCard(String cid);
 
     /**
      * Returns the amount of prestige points a player has.
      *
      * @return the amount of prestige points a player has
      */
-    public int getPrestigePoints() {
-        return this.prestigePoints;
-    }
-
-    @Override
-    public String toString() {
-        return "Player name: " + name + " / Player colour: " + colour;
-    }
+    int getPrestigePoints();
 
 }
--- server/src/main/java/ca/hexanome04/splendorgame/model/gameversions/orient/OrientPlayer.java
+++ server/src/main/java/ca/hexanome04/splendorgame/model/gameversions/orient/OrientPlayer.java
@@ -1,11 +1,26 @@
 package ca.hexanome04.splendorgame.model.gameversions.orient;
 
 import ca.hexanome04.splendorgame.model.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A concrete implementation of Player for Orient game.
  */
-public class OrientPlayer extends Player {
+public class OrientPlayer implements Player {
+
+    private String name;
+    private String colour;
+
+    private int prestigePoints;
+    private HashMap<TokenType, Integer> tokens;
+    private HashMap<TokenType, Integer> bonuses;
+    private ArrayList<DevelopmentCard> devCards;
+    private ArrayList<NobleCard> nobleCards;
+    private ArrayList<DevelopmentCard> reservedCards;
+    private ArrayList<NobleCard> reservedNobles;
 
     /**
      * Creates a Player object with a given name and colour.
@@ -14,7 +29,200 @@ public class OrientPlayer extends Player {
      * @param colour The color associated to the player.
      */
     public OrientPlayer(String name, String colour) {
-        super(name, colour);
+        this.name = name;
+        this.colour = colour;
+        prestigePoints = 0;
+        tokens = new HashMap<>();
+        bonuses = new HashMap<>();
+        devCards = new ArrayList<>();
+        reservedCards = new ArrayList<>();
+        reservedNobles = new ArrayList<>();
+        nobleCards = new ArrayList<>();
+
+        for (TokenType tokenType : TokenType.values()) {
+            tokens.put(tokenType, 0);
+            bonuses.put(tokenType, 0);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getColour() {
+        return colour;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public void addTokens(HashMap<TokenType, Integer> tokens) {
+        tokens.forEach((key, value) -> this.tokens.put(key, this.tokens.get(key) + value));
+    }
+
+    @Override
+    public void addBonus(TokenType bonus, int numBonuses) {
+        this.bonuses.put(bonus, bonuses.get(bonus) + numBonuses);
+    }
+
+    @Override
+    public void addNoble(NobleCard nobleCard) {
+        this.nobleCards.add(nobleCard);
+        this.prestigePoints += 3;
+    }
+
+    @Override
+    public void addPrestigePoints(int prestigePoints) {
+        this.prestigePoints += prestigePoints;
+    }
+
+    @Override
+    public void addCard(DevelopmentCard card) {
+        devCards.add(card);
+    }
+
+    @Override
+    public void removeCard(DevelopmentCard card) {
+        this.devCards.remove(card);
+    }
+
+    @Override
+    public void removeReservedCard(DevelopmentCard card) {
+        this.reservedCards.remove(card);
+    }
+
+    @Override
+    public void removeReservedNoble(NobleCard card) {
+        this.reservedNobles.remove(card);
+    }
+
+    @Override
+    public HashMap<TokenType, Integer> getTokens() {
+        return new HashMap<>(this.tokens);
+    }
+
+    @Override
+    public ArrayList<NobleCard> getNobles() {
+        return new ArrayList<>(this.nobleCards);
+    }
+
+    @Override
+    public List<DevelopmentCard> getReservedCards() {
+        return new ArrayList<>(this.reservedCards);
+    }
+
+    @Override
+    public List<NobleCard> getReservedNobles() {
+        return new ArrayList<>(this.reservedNobles);
+    }
+
+    @Override
+    public Map<TokenType, Integer> getBonuses() {
+        return new HashMap<>(this.bonuses);
+    }
+
+    @Override
+    public void removeTokens(HashMap<TokenType, Integer> tokens) {
+        tokens.forEach((key, value) -> this.tokens.put(key, this.tokens.get(key) - value));
+    }
+
+    @Override
+    public void removeBonuses(HashMap<TokenType, Integer> bonuses) {
+        bonuses.forEach((key, value) -> {
+            Integer currentValue = this.bonuses.get(key);
+            this.bonuses.put(key, currentValue - value);
+        });
+    }
+
+    @Override
+    public void takeTokens(HashMap<TokenType, Integer> take, HashMap<TokenType, Integer> putBack) {
+        addTokens(take);
+        removeTokens(putBack);
+    }
+
+    @Override
+    public void reserveCard(DevelopmentCard card) {
+        reservedCards.add(card);
+    }
+
+    @Override
+    public void reserveNoble(NobleCard noble) {
+        reservedNobles.add(noble);
+    }
+
+    @Override
+    public boolean hasTokens(HashMap<TokenType, Integer> checkTokens) {
+
+        for (TokenType key : checkTokens.keySet()) {
+
+            if ((this.tokens.get(key) - checkTokens.get(key)) < 0) {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean hasBonuses(HashMap<TokenType, Integer> checkBonuses) {
+        for (TokenType key : checkBonuses.keySet()) {
+            if ((this.bonuses.get(key) - checkBonuses.get(key)) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean hasNobles() {
+        return (this.nobleCards.size() > 0);
+    }
+
+    @Override
+    public List<DevelopmentCard> getDevCards() {
+        return new ArrayList<DevelopmentCard>(this.devCards);
+    }
+
+    @Override
+    public DevelopmentCard getPurchasedDevelopmentCard(String cid) {
+        for (DevelopmentCard c : this.devCards) {
+            if (c.getId().equals(cid)) {
+                return c;
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public DevelopmentCard getReservedDevelopmentCard(String cid) {
+        for (DevelopmentCard c : this.reservedCards) {
+            if (c.getId().equals(cid)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getPrestigePoints() {
+        return this.prestigePoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Player name: " + name + " / Player colour: " + colour;
     }
 
 }
